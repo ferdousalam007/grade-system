0a1209ced988b4da90fee3fa3c75c920a02cb14f
ferdousalam007 <iamferdousalam007@gmail.com>
2022-02-07 11:54:28 +0600

    first commit

---
+++ grade.js
@@ -0,0 +1,23 @@
+function grade(marks){
+if(marks<=32){
+ return (`you are failed your total marks is ${marks}`)
+}
+else if(marks<=39){
+    return (`you got C grade your total marks is ${marks}`)
+}
+else if(marks<=49){
+    return (`you got B grade your total marks is ${marks}`)
+}
+else if(marks<=59){
+    return (`you got A- grade your total marks is ${marks}`)
+}
+else if(marks<=79){
+    return (`you got A grade your total marks is ${marks}`)
+}
+else if(marks<=100){
+    return (`you got A+ grade your total marks is ${marks}`)
+}
+
+}
+const yourGrade= grade(33);
+console.log(yourGrade)
